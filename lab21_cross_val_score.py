d7c18b97a1c1a242fb7aac05a0859b286ddceaf8
c1616 <hungpei.lin@gmail.com>
2020-08-22 15:07:41 +0800

    linear regression finish again

---
+++ lab21_cross_val_score.py
@@ -0,0 +1,16 @@
+import sklearn.datasets as datasets
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+print(list(iris.keys()))
+data = iris.data
+target = iris.target
+
+logisticRegression1 = LogisticRegression()
+scores = model_selection.cross_val_score(logisticRegression1,
+                                         data,
+                                         target,
+                                         cv=5)
+print(scores)
+print(scores.mean(), scores.std())
